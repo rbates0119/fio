e7c6f8b4d3b5a6bd953d0b481cf8f8cc9e521b8f
Song Liu <songliubraving@fb.com>
2020-05-17 22:39:49 -0700

    Add option latency_run to continue enable latency_target
    Currently, latency_target run will exist once fio find the highest queue
    depth that meets latency_target. Add option latency_run. If set, fio will
    continue running and try to meet latency_target by adusting queue depth.
    
    Signed-off-by: Song Liu <songliubraving@fb.com>

--- thread_options.h
+++ thread_options.h
@@ -326,6 +326,7 @@ struct thread_options {
 	unsigned long long latency_target;
 	unsigned long long latency_window;
 	fio_fp64_t latency_percentile;
+	uint32_t latency_run;
 
 	unsigned int sig_figs;
 
@@ -631,6 +632,7 @@ struct thread_options_pack {
 	uint64_t latency_window;
 	uint64_t max_latency;
 	fio_fp64_t latency_percentile;
+	uint32_t latency_run;
 
 	uint32_t sig_figs;
 
